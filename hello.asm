5572158ab7afbae9d20847d2d19c175bb71eee65
奔小康 <peengtao123@126.com>
2025-02-04 21:16:35 +0800

    添加注释

--- hello.asm
+++ hello.asm
@@ -1,23 +1,23 @@
 section .data
-    hello db 'Hello, World!', 0
+    hello db 'Hello, World!', 0  ; 定义一个字符串 "Hello, World!" 并以空字符结尾
 
 section .text
-    global main
-    extern printf
+    global main  ; 声明 main 函数为全局符号
+    extern printf  ; 声明外部函数 printf
 
 main:
     ; 设置栈帧
-    push rbp
-    mov rbp, rsp
+    push rbp  ; 保存旧的基址指针
+    mov rbp, rsp  ; 设置新的基址指针
     ; 确保栈是 16 字节对齐的
-    sub rsp, 32
+    sub rsp, 32  ; 为栈帧预留 32 个字节的空间，并确保栈对齐
     ; 设置参数
-    lea rcx, [rel hello]
+    lea rcx, [rel hello]  ; 将字符串 "Hello, World!" 的地址加载到 rcx 寄存器中
     ; 调用 printf
-    call printf
+    call printf  ; 调用 printf 函数
     ; 清理栈帧
-    add rsp, 32
-    mov rsp, rbp
-    pop rbp
+    add rsp, 32  ; 恢复栈指针
+    mov rsp, rbp  ; 恢复旧的栈指针
+    pop rbp  ; 恢复旧的基址指针
     ; 返回
-    ret
+    ret  ; 返回到调用者
