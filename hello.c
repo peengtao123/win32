48d01a3b95bea8b3b6e37d565ff0a1fdc314c772
YouName <you@example.com>
2025-03-03 18:14:36 +0800

    int

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    MessageBox(NULL, "Hello, Windows!", "Message Box", MB_OK | MB_ICONINFORMATION);
-    return 0;
-}
